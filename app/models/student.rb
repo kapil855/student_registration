d503125707eaefd2dd9d78fcd435cec056ff9bee
kapil855 <kapilgurjar4694@gmail.com>
2021-03-09 13:27:28 +0530

    show_full_name and add dropdown

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,38 @@
 class Student < ApplicationRecord
+	# constants for students
+	COURSE_LIST = [
+		'Full stack web development using Ruby on Rails',
+		'Full stack web development using NodeJs',
+		'Full stack web development using Python',
+		'Full stack web development using ReactJs',
+		'Full stack web development using AngularJs',
+		'Back-end web development using Python',
+		'Back-end web development using Ruby on Rails',
+		'Front-end web development',
+		'Basic programing language C / C++'
+	].freeze
+
+	QUALIFICATION = [
+		'B.Tech(Computer Science)',
+		'M.Tech(Computer Science)',
+		'BE(Computer Science)',
+		'ME(Computer Science)',
+		'B.Sc(Computer Science)',
+		'M.Sc(Computer Science)',
+		'BCA',
+		'MCA',
+		'Diploma (Computer Science)',
+		'ITI (Computer Science)'
+	].freeze
+
+	validates :first_name, :last_name, presence: { message: 'must be given please' }
+	validates :email, format: { with: /\A[^@\s]+@[^@\s]+\z/ }, presence: true
+	validates :contact_no,length: {is: 10}, format: {with: /[0-9]/},presence: true
+	validates :date_of_birth,:date_of_joining, presence: true
+	def full_name
+  	#"#{first_name} #{middle_name} #{last_name}"
+  	[first_name, middle_name, last_name].join(' ').capitalize
+	end
 end
+
+
--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module StudentManagement
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
+    # Date::DATE_FORMATS[:default] = "%d %b %Y"
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
